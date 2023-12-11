15bb025bb8c57877db6e777f773d8d1c7eb8e68c
Pertoldi <antoine.pertoldi@gmail.com>
2023-12-11 09:46:25 +0100

    :sparkles: Angular 17

--- front/src/app/pages/home/home.component.ts
+++ front/src/app/pages/home/home.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+  imports: [MatButtonModule]
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  constructor() { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
   start() {
     alert('Commencez par lire le README et à vous de jouer !');
