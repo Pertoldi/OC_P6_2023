97e39401e60afc1e31273b77babab61d21fd1de8
Pertoldi <antoine.pertoldi@gmail.com>
2023-12-12 17:57:54 +0100

    :sparkles: Comment routes

--- back/src/main/java/mdd/api/back/repository/ArticleRepository.java
+++ back/src/main/java/mdd/api/back/repository/ArticleRepository.java
@@ -5,9 +5,11 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import mdd.api.back.model.Article;
 
+@Repository
 public interface ArticleRepository extends JpaRepository<Article, Integer> {
 
   @Query("SELECT a FROM Article a LEFT JOIN FETCH a.comments WHERE a.id = :id")
---
+++ back/src/main/java/mdd/api/back/request/CommentRequest.java
@@ -0,0 +1,15 @@
+package mdd.api.back.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentRequest {
+  private String content;
+  private Integer articleId;
+}
