5701442466116f6691fcbcd7191a9e3207eab525
Pertoldi <antoine.pertoldi@gmail.com>
2024-01-30 23:08:14 +0100

    :sparkles: user-profile component (#14)

--- front/src/app/pages/user-profile/user-profile.component.ts
+++ front/src/app/pages/user-profile/user-profile.component.ts
@@ -1,35 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderPrivateComponent } from '../../core/components/header-private/header-private.component';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { ITheme } from '../../core/components/themes/theme.model';
 import { ThemesComponent } from '../../core/components/themes/themes.component';
-import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-themes-list',
+  selector: 'app-user-profile',
   standalone: true,
-  imports: [HeaderPrivateComponent, ThemesComponent, CommonModule],
-  templateUrl: './themes-list.component.html',
-  styleUrl: './themes-list.component.scss'
+  imports: [HeaderPrivateComponent, CommonModule, ReactiveFormsModule, MatButtonModule, ThemesComponent],
+  templateUrl: './user-profile.component.html',
+  styleUrl: './user-profile.component.scss'
 })
-export class ThemesListComponent {
+export class UserProfileComponent implements OnInit {
+  profileForm!: FormGroup;
   themes: ITheme[] = [
     {
       id: 1,
       content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis maxime aperiam rerum soluta quae! Dicta aperiam magnam dolor facilis molestias voluptatum vero maiores! Non dolorum saepe explicabo ipsam nostrum odit.",
       title: "Java",
-      isSubscribe: false
+      isSubscribe: true
     },
     {
       id: 2,
       content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis maxime aperiam rerum soluta quae! Dicta aperiam magnam dolor facilis molestias voluptatum vero maiores! Non dolorum saepe explicabo ipsam nostrum odit.",
       title: "PHP",
-      isSubscribe: false
+      isSubscribe: true
     },
     {
       id: 2,
       content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis maxime aperiam rerum soluta quae! Dicta aperiam magnam dolor facilis molestias voluptatum vero maiores! Non dolorum saepe explicabo ipsam nostrum odit.",
-      title: "Javascript",
-      isSubscribe: false
+      title: "PHP",
+      isSubscribe: true
     }
   ];
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) { }
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm() {
+    this.profileForm = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  onSubmit() {
+
+  }
 }
