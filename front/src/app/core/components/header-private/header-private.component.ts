6bd2cdcb4f2a1c86ccac17d0ccc8e093276a0296
Pertoldi <antoine.pertoldi@gmail.com>
2024-01-30 23:14:09 +0100

    WIP

--- front/src/app/core/components/header-private/header-private.component.ts
+++ front/src/app/core/components/header-private/header-private.component.ts
@@ -1,19 +1,24 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header-private',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, RouterLink],
   templateUrl: './header-private.component.html',
   styleUrl: './header-private.component.scss'
 })
 export class HeaderPrivateComponent {
 
-  active = '';
+  active = this.router.url.split('/')[0];
+
+  constructor(private router: Router) {
+
+    console.log('active is :', this.active)
+  }
 
   setActive(label: string) {
-    this.active = label;
   }
 
 }
