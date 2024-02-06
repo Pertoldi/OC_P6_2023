374f54cd94f1d09d616bd06a5aa3dbb16b475df2
Pertoldi <antoine.pertoldi@gmail.com>
2024-02-06 16:22:44 +0100

    :sparkles: Articles

--- front/src/app/pages/articles/articles.component.ts
+++ front/src/app/pages/articles/articles.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ArticlesComponentComponent } from "../../core/components/articles-component/articles-component.component";
 import { HeaderPrivateComponent } from '../../core/components/header-private/header-private.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 import { Router, RouterLink, RouterModule } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { ArticlesService } from '../../core/services/articles.service';
 
 @Component({
   selector: 'app-articles',
@@ -13,25 +15,52 @@ import { Router, RouterLink, RouterModule } from '@angular/router';
   styleUrl: './articles.component.scss',
   imports: [ArticlesComponentComponent, CommonModule, HeaderPrivateComponent, MatButtonModule, MatIconModule, RouterLink]
 })
-export class ArticlesComponent {
-  articles = [
-    {
-      id: 1,
-      title: 'Titre',
-      createdAt: new Date(),
-      content: 'Content: lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled... lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scramble',
-      author: {
-        name: 'Toto',
+
+export class ArticlesComponent implements OnInit, OnDestroy {
+
+  private subscription = new Subscription();
+
+
+  constructor(
+    private articlesService: ArticlesService
+  ) {
+
+  }
+
+  articles: any[] = []
+  //  [
+  //   {
+  //     id: 1,
+  //     title: 'Titre',
+  //     createdAt: new Date(),
+  //     content: 'Content: lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled... lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scramble',
+  //     author: {
+  //       name: 'Toto',
+  //     }
+  //   },
+  //   {
+  //     id: 2,
+  //     title: 'Titre 2',
+  //     createdAt: new Date(),
+  //     content: 'Content: lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled... lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scramble',
+  //     author: {
+  //       name: 'Toto',
+  //     }
+  //   }
+  // ];
+
+  ngOnInit(): void {
+    const articlesSubscription = this.articlesService.getAll().subscribe({
+      next: (response: any) => {
+        this.articles = response
+      },
+      error: (error) => {
       }
-    },
-    {
-      id: 2,
-      title: 'Titre 2',
-      createdAt: new Date(),
-      content: 'Content: lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled... lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scramble',
-      author: {
-        name: 'Toto',
-      }
-    }
-  ];
+    });
+    this.subscription.add(articlesSubscription);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
--- front/src/app/pages/new-article/new-article.component.ts
+++ front/src/app/pages/new-article/new-article.component.ts
@@ -4,34 +4,66 @@ import { MatIconModule } from '@angular/material/icon';
 import { FormGroup, ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { Router, RouterLink } from '@angular/router';
+import { ArticlesService } from './../../core/services/articles.service';
+import { Subscription } from 'rxjs';
+import { SubjectsService } from '../../core/services/subjects.service';
 
 @Component({
   selector: 'app-new-article',
   standalone: true,
-  imports: [HeaderPrivateComponent, MatIconModule, ReactiveFormsModule, CommonModule, MatButtonModule],
+  imports: [HeaderPrivateComponent, MatIconModule, ReactiveFormsModule, CommonModule, MatButtonModule, RouterLink],
   templateUrl: './new-article.component.html',
   styleUrl: './new-article.component.scss'
 })
 export class NewArticleComponent implements OnInit {
   newArticleForm!: FormGroup;
-  themeList = ['PHP', 'JS'];
+  private subscription = new Subscription();
+
+
+  subjects: any[] = [];
 
   constructor(
+    private articlesService: ArticlesService,
+    private subjectsService: SubjectsService,
+    private router: Router,
     private formBuilder: FormBuilder,
   ) { }
 
   ngOnInit() {
     this.initForm();
+    this.subjectsService.getAll().subscribe({
+      next: (response: any) => {
+        this.subjects = response
+      },
+      error: (error) => {
+      }
+    });
   }
   initForm() {
     this.newArticleForm = this.formBuilder.group({
-      theme: ['', [Validators.required]],
+      subjectId: [0, [Validators.required]],
       title: ['', [Validators.required]],
       content: ['', [Validators.required]]
     });
   }
 
   onSubmit() {
+    const formValue = this.newArticleForm.value;
+    console.log('formValue is :', formValue)
+    const registerSubscription = this.articlesService.create(formValue).subscribe({
+      next: (response: any) => {
+        this.router.navigate(['/articles']);
+      },
+      error: (error: any) => {
+        //TODO a toast
+        console.error('Login error:', error);
+      }
+    });
+    this.subscription.add(registerSubscription);
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
